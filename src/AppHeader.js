4f4d78a95075232ceff6d0d10cd60773cafa7ab8
Natarajan <nataraj.itsolution@gmail.com>
2022-09-17 13:21:17 +0530

    usestate hook

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const AppHeader = () => {
+    //let titletext="Welcome to Nihira Techiees";
+   const[titletext,changetext] =useState("Welcome to Nihira Techiees")
+   const[userinfo,changeuser]=useState({'name':'robert','age':'25'});
+    const handleclick=()=>{
+      //  titletext="Welcome to React Tutorial";
+      changetext("Welcome to React Tutorial")
+      changeuser({'name':'Alex','age':'29'})
+        console.log(titletext);
+    };
+
+    
     return ( 
         <div>
-            <h2>Welcome to Nihira Techiees</h2>
+            <h2>{titletext}</h2>
+            <h3>user name is {userinfo.name} and his age is : {userinfo.age}</h3>
+            <button onClick={handleclick}>Change title</button>
         </div>
      );
 }
